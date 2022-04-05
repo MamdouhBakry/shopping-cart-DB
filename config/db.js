59b13a120117aeeabfd3bbc25d0682694ff0d79d
mamdouh <mamdouh>
2022-04-05 07:33:51 +0200

    add process.env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const env = require("dotenv");
 const runDb = () => {
     mongoose.connect(
-        `mongodb+srv://root:admin@cluster0.ipjag.mongodb.net/shoping_cart?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.ipjag.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
     )
         .then(() => {
             console.log("Database Connected");
